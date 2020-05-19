4fbd9f2db51b376ed761a6cb3dd98e7de5c7fa93
ColeyG <cjgeerts@gmail.com>
2020-05-18 22:26:56 -0400

    Initial Project Structure and Hello World!

---
+++ README.md
@@ -0,0 +1,9 @@
+# Learning SDL 2
+
+## Building
+
+### Linux
+
+The dependencies for linux can be sufficed with the following command:
+
+- `sudo apt install cmake libsdl2-dev g++`
--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <SDL.h>
 
-int main(int, char **)
+SDL_Window *g_pWindow = 0;
+SDL_Renderer *g_pRenderer = 0;
+
+int main(int argc, char *args[])
 {
-  std::cout << "Hello, world!\n";
+  // std::cout << "Hello, world!\n";
+  if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+  {
+    g_pWindow = SDL_CreateWindow("Asdf", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 500, SDL_WINDOW_SHOWN);
+
+    if (g_pWindow != 0)
+    {
+      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+    }
+    else
+    {
+      std::cout << "SDL Could not initialize\n";
+      return 1;
+    }
+
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+    SDL_RenderPresent(g_pRenderer);
+
+    SDL_Delay(5000);
+    SDL_Quit();
+
+    return 0;
+  }
 }
